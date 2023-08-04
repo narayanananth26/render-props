1e60e3e9d6ff862c08bc2a71c49f3a139539771e
Ananth Narayan <narayanananth26@gmail.com>
2023-08-04 12:46:37 +0530

    implement the render prop

--- src/App.js
+++ src/App.js
@@ -28,13 +28,13 @@ function ProductItem({ product }) {
 }
 
 function CompanyItem({ company, defaultVisibility }) {
-	const [isVisible, setIsVisisble] = useState(defaultVisibility);
+	const [isVisible, setIsVisible] = useState(defaultVisibility);
 
 	return (
 		<li
 			className="company"
-			onMouseEnter={() => setIsVisisble(true)}
-			onMouseLeave={() => setIsVisisble(false)}
+			onMouseEnter={() => setIsVisible(true)}
+			onMouseLeave={() => setIsVisible(false)}
 		>
 			<p className="company-name">{company.companyName}</p>
 			{isVisible && (
@@ -46,7 +46,7 @@ function CompanyItem({ company, defaultVisibility }) {
 	);
 }
 
-function List({ title, items }) {
+function List({ title, items, render }) {
 	const [isOpen, setIsOpen] = useState(true);
 	const [isCollapsed, setIsCollapsed] = useState(false);
 
@@ -65,16 +65,7 @@ function List({ title, items }) {
 					{isOpen ? <span>&or;</span> : <span>&and;</span>}
 				</button>
 			</div>
-			{isOpen && (
-				<ul className="list">
-					{displayItems.map((product) => (
-						<ProductItem
-							key={product.productName}
-							product={product}
-						/>
-					))}
-				</ul>
-			)}
+			{isOpen && <ul className="list">{displayItems.map(render)}</ul>}
 
 			<button
 				onClick={() => setIsCollapsed((isCollapsed) => !isCollapsed)}
@@ -91,7 +82,27 @@ export default function App() {
 			<h1>Render Props Demo</h1>
 
 			<div className="col-2">
-				<List title="Products" items={products} />
+				<List
+					title="Products"
+					items={products}
+					render={(product) => (
+						<ProductItem
+							key={product.productName}
+							product={product}
+						/>
+					)}
+				/>
+				<List
+					title="Companies"
+					items={companies}
+					render={(company) => (
+						<CompanyItem
+							key={company.companyName}
+							company={company}
+							defaultVisibility={false}
+						/>
+					)}
+				/>
 			</div>
 		</div>
 	);
